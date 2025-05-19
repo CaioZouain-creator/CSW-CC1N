6a8c411cee6858797549f99e53ea1c2564d8317e
CaioZouain-creator <goltarazouaincaio@gmail.com>
2025-05-19 20:45:08 -0300

    Add files via upload

--- aula 19/script.js
+++ aula 19/script.js
@@ -1,7 +1,10 @@
+
+function VerificarIdade(){
 let idade= window.prompt("qual sua idade?");
 idade=parseInt(idade);
 if (idade <10) { window.alert("você é criança")
     
 } else { Window.alert("Você é adulto")
     
+}
 }
